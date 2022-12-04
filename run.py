8b5dc4cc89bfb4d3c4f7e216d2fbe206b3101872
CardenasGalarza <alex10estadistica@gmail.com>
2022-12-04 17:18:20 -0500

    wefwef

--- run.py
+++ run.py
@@ -1,12 +1,43 @@
 import os
 import sys
 import streamlit.web.cli as stcli
+import streamlit as st
+import mysql.connector
+from mysql.connector import Error
+import time
+from datetime import datetime
+import streamlit as st
+import time
+import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import warnings
 warnings.filterwarnings('ignore')
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pickle
+from pathlib import Path
+import pandas as pd
+import numpy as np
+#from soupsieve import select  # pip install pandas openpyxl
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+############################################ OCULTAR INFROMACION NO IMPORTANTE
+import base64
+import mysql.connector
+from mysql.connector import Error
+#import pyodbc
+import streamlit as st
+############################################ OCULTAR INFROMACION NO IMPORTANTE
+import warnings
+warnings.filterwarnings('ignore')
+#########################################3333
+##########################
+import time
+from datetime import datetime
+from datetime import timedelta
 
 if __name__ == '__main__':
 
