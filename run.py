551fa059135f3537694a1951de9365c21a741491
CardenasGalarza <alex10estadistica@gmail.com>
2022-12-04 14:50:13 -0500

    aaddad

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import streamlit.web.cli as stcli
+import sys
+
+if __name__ == '__main__':
+
+    launchdir = os.path.dirname(sys.argv[0])
+
+    #if launchdir == '':
+    #    launchdir = '.'
+
+    print(launchdir)
+    sys.argv = ["streamlit", "run", "https://raw.githubusercontent.com/CardenasGalarza/mensaje/main/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
+    #sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--global.developmentMode=false"]
+    sys.exit(stcli.main())
