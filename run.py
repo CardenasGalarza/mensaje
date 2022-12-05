5836918ba755138ca4390d2f0d6ca6500e2dca4f
CardenasGalarza <alex10estadistica@gmail.com>
2022-12-04 19:11:29 -0500

    sddsd

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import os
 import sys
 import streamlit.web.cli as stcli
-import streamlit as st
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import warnings
+warnings.filterwarnings('ignore')
+#####3333
+##########################
+import time
+from datetime import datetime
 
 if __name__ == '__main__':
 
@@ -12,7 +20,7 @@ if __name__ == '__main__':
     #    launchdir = '.'
 
     print(launchdir)
-    sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
-    #sys.argv = ["streamlit", "run", "https://raw.githubusercontent.com/CardenasGalarza/mensaje/main/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
+    #sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
+    sys.argv = ["streamlit", "run", "https://raw.githubusercontent.com/CardenasGalarza/mensaje/main/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
     #sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--global.developmentMode=false"]
     sys.exit(stcli.main())
